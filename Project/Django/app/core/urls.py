207d574458fd0f476a8168feb8cd25f0572191af
Iryna Gychka <iryna.gychka.triare@gmail.com>
2026-04-24 19:36:16 +0300

    Fix Django views import

--- Project/Django/app/core/urls.py
+++ Project/Django/app/core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from Project.Django.app.main.views import home_view, health_view
+from main.views import home_view, health_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
